3b2ae08f004651b3e3c3d64b396489db29ef4566
saibharath-16 <108922762+saibharath-16@users.noreply.github.com>
2024-07-24 17:18:53 +0530

    HttpModule.js

--- HttpModule.js
+++ HttpModule.js
@@ -1,37 +1,50 @@
-const http=require("http");
-const fs=require("fs")
-const url=require("url")
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-const myServer=http.createServer((req,res)=>{
-    const log=`${req.method} ${req.url} New Req Received \n`
-    // console.log(` New Request Received from user \n`);
-    const myUrl=url.parse(req.url,true)
-    console.log(myUrl)
-    // res.write("hello user")
-    // res.end("Hello Message from server")
-    fs.appendFile("./log.txt",log,(err,data)=>{
 
-        switch(myUrl.pathname){
-            case "/" :
-                res.end("Home Page");
-            break
-            
-            case "/about" :
-                const username=myUrl.query.myname;    
-                res.end(`Hi, ${username}`)
-            break
+let eventEmitter=new eventE.EventEmitter();
 
-            case "/search":
-                const search=myUrl.query.search_query;
-                res.end("You have searched for "+ search);    
+const myServer = http.createServer((req, res) => {
+    const log = `${req.method} ${req.url} New Req Received \n`;
+    const baseUrl = `http://${req.headers.host}`; 
+    const myUrl = url.parse(baseUrl + req.url, true);
+    console.log(req.headers)
+    
+    switch (myUrl.pathname) {
+        case "/":
+            console.log(myUrl);
+            res.end("Home Page");
 
-            default:res.end("404 Page not found")
+            break;
+
+        case "/about":
+            const username = myUrl.query.name; 
+            console.log(myUrl);
+            res.end(`Hi, ${username}`);
+            break;
+
+        case "/search":
+            console.log('Search Page Query:', myUrl);
+            res.end("You are in  search ");
+            break;
+    
+
+        default:
+            res.end("404 Page not found");
+    }
+
+    
+    fs.appendFile("./log.txt", log, (err) => {
+        if (err) {
+            console.error("Error logging request:", err);
         }
-        
-    })
+    });
 });
-myServer.listen(2000,()=>{
-    console.log("Server is Running");
-})
 
-// if we want to change anything . then we need to restart the server to see the results
+myServer.listen(2000, () => {
+    console.log("Server is Running");
+});
+
+//If we want to change anything, then we need to restart the server to see the results.
+
